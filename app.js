85002cc8d0a073bac1aab1b9724fc8a86e6887cb
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-22 12:39:17 -0500

    POST Crea un endpoint que se encargue de crear un explorer

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.get('/v1/explorers/:id', (req, res) => {
   res.status(200).json(explorer);
 })
 
+app.post('/v1/explorers', (req, res) => {
+  console.log(`Api explorers POST request ${new Date()}`);
+  const requestBody = req.body // Parámetros de un cliente
+  res.status(200).json({massage: "Created"});
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
