0306dbd221b49b0e412994f9134a59e117df2e85
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-04-22 12:36:18 -0500

    GET Crea un endpoint que te regrese un explorer mediando un ID

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.get('/v1/explorers', (req, res) => {
   res.status(200).json(explorers);
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+  console.log(`Api explorers get request ${new Date()}`);
+  console.log(`Getting explorer with id ${req.params.id}`);
+  const explorer = {id: 1, name: "Carlos"};
+  res.status(200).json(explorer);
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
